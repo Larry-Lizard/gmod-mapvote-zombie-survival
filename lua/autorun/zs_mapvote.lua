feb8e12d618890a859a7de2312a90df835b25528
Larry-Lizard <poroofficial@freenet.de>
2016-02-20 15:45:27 +0100

    Changed mapvote.lua

--- lua/autorun/zs_mapvote.lua
+++ lua/autorun/zs_mapvote.lua
@@ -1,7 +1,7 @@
 local settings = {
-	Length = 15, -- Vote lasts 24 seconds
-	AllowCurrent = true, -- Don't allow current map to be re-voted
-	Limit = 20, -- Only allow the choice of 8 maps
+	Length = 24, -- Vote lasts 24 seconds
+	AllowCurrent = false, -- Don't allow current map to be re-voted
+	Limit = 8, -- Only allow the choice of 8 maps
 	Prefix = {"zs_", "ze_"}, -- Only allow maps beginning with ttt_ and cs_italy
 }
 
@@ -11,3 +11,12 @@ hook.Add( "LoadNextMap", "MapVote", function()
 MapVote.Start(settings.Length, settings.AllowCurrent, settings.Limit, settings.Prefix)
 return true 
 end )
+
+if SERVER then
+    AddCSLuaFile()
+    AddCSLuaFile("mapvote/cl_mapvote.lua")
+
+    include("mapvote/sv_mapvote.lua")
+else
+    include("mapvote/cl_mapvote.lua")
+end
